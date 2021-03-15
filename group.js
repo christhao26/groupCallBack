079cca964004f11123dff74bb5a0dc79d03effeb
christhao26 <79154880+christhao26@users.noreply.github.com>
2021-03-15 16:50:26 -0400

    intial commit

---
+++ group.js
@@ -0,0 +1,36 @@
+const posts = [
+    { title: 'Phase 1', body: 'Collect underpants' },
+    { title: 'Phase 2', body: '?' },
+];
+​
+function getPosts(){
+​
+    setTimeout(function() {
+        
+        setTimeout(() => {
+            let output = '';
+            posts.forEach((post) => {
+                output += `<li>${post.title}: ${post.body}</li>`;
+            })
+            output += `<button type="button" id="clear-list" onclick="removeAll()">Clear Items</button>`
+            document.body.innerHTML = output;
+        })
+    }, 1000);
+​
+}
+​
+function createPost(post, callback) {
+    
+    setTimeout(()=>{  
+        posts.push(post);
+        callback();
+    },2000)
+​
+}
+document.getElementById("callMe").onclick=()=> {
+    createPost({ title: 'Phase 3', body: 'Profit'}, getPosts);
+}
+​
+function removeAll() {
+    document.body.innerHTML = '';
+}
